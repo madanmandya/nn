a9e578a82570d66ded13c2d859f73895e99df5ea
madanmandya <121280074+madanmandya@users.noreply.github.com>
2022-12-23 12:00:48 +0530

    python

---
+++ madan.py
@@ -0,0 +1,16 @@
+def decrypt(text,s):
+    result=" "
+    
+    for i in range(len(text)):
+        char=text[i]
+        
+        if(char.isupper()):
+            result+=chr((ord(char)+s-65)%26+65)
+        else:
+            result+=chr((ord(char)+s-97)%26+97)
+    return result
+text="EXXEGOW"
+s=4
+print("text:"+text)
+print("shift:"+str(s))
+print("Cipher:"+decrypt(text,s))
